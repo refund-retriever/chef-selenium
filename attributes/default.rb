0face1c2647ea5f67b073147920f2207c0223b20
Billy Cravens <bdcravens@gmail.com>
2014-05-31 00:55:36 -0500

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['selenium']['server']['version'] = "2.31.0"
+default['selenium']['server']['version'] = "2.41.0"
 default['selenium']['server']['user'] = "selenium"
 default['selenium']['server']['installpath'] = "/usr/share/selenium"
 default['selenium']['server']['logpath'] = "/var/log/selenium"
