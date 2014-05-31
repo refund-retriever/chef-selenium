3d9617636a5a2dfbe08046f8a6b7f2fd69977be3
Billy Cravens <bdcravens@gmail.com>
2014-05-31 00:57:13 -0500

    update with new Selenium download location

--- recipes/server.rb
+++ recipes/server.rb
@@ -3,7 +3,8 @@ include_recipe 'selenium::default'
 directory node['selenium']['server']['installpath']
 
 remote_file File.join(node['selenium']['server']['installpath'], 'selenium-server-standalone.jar') do
-  source "http://selenium.googlecode.com/files/selenium-server-standalone-#{node['selenium']['server']['version']}.jar"
+  #source "http://selenium.googlecode.com/files/selenium-server-standalone-#{node['selenium']['server']['version']}.jar"
+  source "http://selenium-release.storage.googleapis.com/2.42/selenium-server-standalone-2.42.1.jar"
   action :create
   mode 0644
 end
