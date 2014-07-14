d499eadd12f56e1e406ce2a503252f313b6dfc24
Restless-ET <restless86@gmail.com>
2014-07-14 14:13:54 +0100

    Fixed selenium server recipe to properly support version given as attribute

--- recipes/server.rb
+++ recipes/server.rb
@@ -2,9 +2,17 @@ include_recipe 'selenium::default'
 
 directory node['selenium']['server']['installpath']
 
+splittedversion = node['selenium']['server']['version'].split('.')
+majorversion = splittedversion[0] + '.' + splittedversion[1]
+
+if majorversion.to_f >= 2.39
+  url = "http://selenium-release.storage.googleapis.com/#{majorversion}/selenium-server-standalone-#{node['selenium']['server']['version']}.jar"
+else
+  url = "http://selenium.googlecode.com/files/selenium-server-standalone-#{node['selenium']['server']['version']}.jar"
+end
+
 remote_file File.join(node['selenium']['server']['installpath'], 'selenium-server-standalone.jar') do
-  #source "http://selenium.googlecode.com/files/selenium-server-standalone-#{node['selenium']['server']['version']}.jar"
-  source "http://selenium-release.storage.googleapis.com/2.42/selenium-server-standalone-2.42.1.jar"
+  source url
   action :create
   mode 0644
 end
